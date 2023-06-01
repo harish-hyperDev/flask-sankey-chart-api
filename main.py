1332785003cc7ed06830d4b35440e06480259fac
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-06-01 14:25:39 +0530

    removed unwanted code... finalizing

--- main.py
+++ main.py
@@ -8,29 +8,10 @@ app = Flask(__name__)
 # Home for multi timelines
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    
-    df = pd.read_csv('static/leadconvert.csv')
-    json_data = df.to_json(orient='records')
-    
-    data_to_dict = json.loads(json_data)[0]
-    
-    # return jsonify(data)
     return render_template('index.html')
 
 
-# Needs to be implemented
-@app.route('/<opp_id>/<type>', methods=['GET', 'POST'])
-def api(opp_id, type):
-    
-    df = pd.read_csv('static/leadconvert.csv')
-    data = df.to_json(orient='records')
-    
-    return render_template('index.html', 
-                           id = opp_id, 
-                        )
-
-
-# For fetching data
+# For sending data to Sankey in JSON format
 @app.route('/get_data', methods=['GET'])
 def send_data():
     
--- static/sankey-new.js
+++ static/sankey-new.js
@@ -395,10 +395,7 @@ function drawSankey(graph) {
 }
 
 
-// load the data
-// d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_sankey.json", function(error, graph) {
-
-// Constructs a new Sankey generator with the default settings.
+//// Helper functions
 
 const onlyUnique = (value, index, array) => {
     return array.indexOf(value) === index;
@@ -423,262 +420,17 @@ const checkNodesAndLinks = (arr) => {
 const getNodes = (arr) => {
     return arr.map(v => { return { "name": v } })
 }
+//// End of Helper functions 
+
+// Load Data
 
 fetch('/get_data').then(res => res.json()).then(responseData => {
 
-    let testData =
-    {
-        "links": [
-            { "source": "Agricultural Energy Use", "target": "Carbon Dioxide", "value": "1.4" },
-            { "source": "Agriculture", "target": "Agriculture Soils", "value": "5.2" },
-            { "source": "Agriculture", "target": "Livestock and Manure", "value": "5.4" },
-            { "source": "Agriculture", "target": "Other Agriculture", "value": "1.7" },
-            { "source": "Agriculture", "target": "Rice Cultivation", "value": "1.5" },
-            { "source": "Agriculture Soils", "target": "Nitrous Oxide", "value": "5.2" },
-            { "source": "Air", "target": "Carbon Dioxide", "value": "1.7" },
-            { "source": "Aluminium Non-Ferrous Metals", "target": "Carbon Dioxide", "value": "1.0" },
-            { "source": "Aluminium Non-Ferrous Metals", "target": "HFCs - PFCs", "value": "0.2" },
-            { "source": "Cement", "target": "Carbon Dioxide", "value": "5.0" },
-            { "source": "Chemicals", "target": "Carbon Dioxide", "value": "3.4" },
-            { "source": "Chemicals", "target": "HFCs - PFCs", "value": "0.5" },
-            { "source": "Chemicals", "target": "Nitrous Oxide", "value": "0.2" },
-            { "source": "Coal Mining", "target": "Carbon Dioxide", "value": "0.1" },
-            { "source": "Coal Mining", "target": "Methane", "value": "1.2" },
-            { "source": "Commercial Buildings", "target": "Carbon Dioxide", "value": "6.3" },
-            { "source": "Deforestation", "target": "Carbon Dioxide", "value": "10.9" },
-            { "source": "Electricity and heat", "target": "Agricultural Energy Use", "value": "0.4" },
-            { "source": "Electricity and heat", "target": "Aluminium Non-Ferrous Metals", "value": "0.4" },
-            { "source": "Electricity and heat", "target": "Cement", "value": "0.3" },
-            { "source": "Electricity and heat", "target": "Chemicals", "value": "1.3" },
-            { "source": "Electricity and heat", "target": "Commercial Buildings", "value": "5.0" },
-            { "source": "Electricity and heat", "target": "Food and Tobacco", "value": "0.5" },
-            { "source": "Electricity and heat", "target": "Iron and Steel", "value": "1.0" },
-            { "source": "Electricity and heat", "target": "Machinery", "value": "1.0" },
-            { "source": "Electricity and heat", "target": "Oil and Gas Processing", "value": "0.4" },
-            { "source": "Electricity and heat", "target": "Other Industry", "value": "2.7" },
-            { "source": "Electricity and heat", "target": "Pulp - Paper and Printing", "value": "0.6" },
-            { "source": "Electricity and heat", "target": "Residential Buildings", "value": "5.2" },
-            { "source": "Electricity and heat", "target": "T and D Losses", "value": "2.2" },
-            { "source": "Electricity and heat", "target": "Unallocated Fuel Combustion", "value": "2.0" },
-            { "source": "Energy", "target": "Electricity and heat", "value": "24.9" },
-            { "source": "Energy", "target": "Fugitive Emissions", "value": "4.0" },
-            { "source": "Energy", "target": "Industry", "value": "14.7" },
-            { "source": "Energy", "target": "Other Fuel Combustion", "value": "8.6" },
-            { "source": "Energy", "target": "Transportation", "value": "14.3" },
-            { "source": "Food and Tobacco", "target": "Carbon Dioxide", "value": "1.0" },
-            { "source": "Fugitive Emissions", "target": "Coal Mining", "value": "1.3" },
-            { "source": "Fugitive Emissions", "target": "Oil and Gas Processing", "value": "3.2" },
-            { "source": "Harvest \/ Management", "target": "Carbon Dioxide", "value": "1.3" },
-            { "source": "Industrial Processes", "target": "Aluminium Non-Ferrous Metals", "value": "0.4" },
-            { "source": "Industrial Processes", "target": "Cement", "value": "2.8" },
-            { "source": "Industrial Processes", "target": "Chemicals", "value": "1.4" },
-            { "source": "Industrial Processes", "target": "Other Industry", "value": "0.5" },
-            { "source": "Industry", "target": "Aluminium Non-Ferrous Metals", "value": "0.4" },
-            { "source": "Industry", "target": "Cement", "value": "1.9" },
-            { "source": "Industry", "target": "Chemicals", "value": "1.4" },
-            { "source": "Industry", "target": "Food and Tobacco", "value": "0.5" },
-            { "source": "Industry", "target": "Iron and Steel", "value": "3.0" },
-            { "source": "Industry", "target": "Oil and Gas Processing", "value": "2.8" },
-            { "source": "Industry", "target": "Other Industry", "value": "3.8" },
-            { "source": "Industry", "target": "Pulp - Paper and Printing", "value": "0.5" },
-            { "source": "Iron and Steel", "target": "Carbon Dioxide", "value": "4.0" },
-            { "source": "Land Use Change", "target": "Deforestation", "value": "10.9" },
-            { "source": "Land Use Change", "target": "Harvest \/ Management", "value": "1.3" },
-            { "source": "Landfills", "target": "Methane", "value": "1.7" },
-            { "source": "Livestock and Manure", "target": "Methane", "value": "5.1" },
-            { "source": "Livestock and Manure", "target": "Nitrous Oxide", "value": "0.3" },
-            { "source": "Machinery", "target": "Carbon Dioxide", "value": "1.0" },
-            { "source": "Oil and Gas Processing", "target": "Carbon Dioxide", "value": "3.6" },
-            { "source": "Oil and Gas Processing", "target": "Methane", "value": "2.8" },
-            { "source": "Other Agriculture", "target": "Methane", "value": "1.4" },
-            { "source": "Other Agriculture", "target": "Nitrous Oxide", "value": "0.3" },
-            { "source": "Other Fuel Combustion", "target": "Agricultural Energy Use", "value": "1.0" },
-            { "source": "Other Fuel Combustion", "target": "Commercial Buildings", "value": "1.3" },
-            { "source": "Other Fuel Combustion", "target": "Residential Buildings", "value": "5.0" },
-            { "source": "Other Fuel Combustion", "target": "Unallocated Fuel Combustion", "value": "1.8" },
-            { "source": "Other Industry", "target": "Carbon Dioxide", "value": "6.6" },
-            { "source": "Other Industry", "target": "HFCs - PFCs", "value": "0.4" },
-            { "source": "Pulp - Paper and Printing", "target": "Carbon Dioxide", "value": "1.1" },
-            { "source": "Rail - Ship and Other Transport", "target": "Carbon Dioxide", "value": "2.5" },
-            { "source": "Residential Buildings", "target": "Carbon Dioxide", "value": "10.2" },
-            { "source": "Rice Cultivation", "target": "Methane", "value": "1.5" },
-            { "source": "Road", "target": "Carbon Dioxide", "value": "10.5" },
-            { "source": "T and D Losses", "target": "Carbon Dioxide", "value": "2.2" },
-            { "source": "Transportation", "target": "Air", "value": "1.7" },
-            { "source": "Transportation", "target": "Rail - Ship and Other Transport", "value": "2.5" },
-            { "source": "Transportation", "target": "Road", "value": "10.5" },
-            { "source": "Unallocated Fuel Combustion", "target": "Carbon Dioxide", "value": "3.0" },
-            { "source": "Unallocated Fuel Combustion", "target": "Methane", "value": "0.4" },
-            { "source": "Unallocated Fuel Combustion", "target": "Nitrous Oxide", "value": "0.4" },
-            { "source": "Waste", "target": "Landfills", "value": "1.7" },
-            { "source": "Waste", "target": "Waste water - Other Waste", "value": "1.5" },
-            { "source": "Waste water - Other Waste", "target": "Methane", "value": "1.2" },
-            { "source": "Waste water - Other Waste", "target": "Nitrous Oxide", "value": "0.3" }
-        ],
-        "nodes": [
-            { "name": "Energy" },
-            { "name": "Industrial Processes" },
-            { "name": "Electricity and heat" },
-            { "name": "Industry" },
-            { "name": "Land Use Change" },
-            { "name": "Agriculture" },
-            { "name": "Waste" },
-            { "name": "Transportation" },
-            { "name": "Other Fuel Combustion" },
-            { "name": "Fugitive Emissions" },
-            { "name": "Road" }, { "name": "Air" },
-            { "name": "Rail - Ship and Other Transport" },
-            { "name": "Residential Buildings" },
-            { "name": "Commercial Buildings" },
-            { "name": "Unallocated Fuel Combustion" },
-            { "name": "Iron and Steel" },
-            { "name": "Aluminium Non-Ferrous Metals" },
-            { "name": "Machinery" },
-            { "name": "Pulp - Paper and Printing" },
-            { "name": "Food and Tobacco" },
-            { "name": "Chemicals" },
-            { "name": "Cement" },
-            { "name": "Other Industry" },
-            { "name": "T and D Losses" },
-            { "name": "Coal Mining" },
-            { "name": "Oil and Gas Processing" },
-            { "name": "Deforestation" },
-            { "name": "Harvest \/ Management" },
-            { "name": "Agricultural Energy Use" },
-            { "name": "Agriculture Soils" },
-            { "name": "Livestock and Manure" },
-            { "name": "Rice Cultivation" },
-            { "name": "Other Agriculture" },
-            { "name": "Landfills" },
-            { "name": "Waste water - Other Waste" },
-            { "name": "Carbon Dioxide" },
-            { "name": "HFCs - PFCs" },
-            { "name": "Methane" },
-            { "name": "Nitrous Oxide" }
-        ]
-    }
-
-    let graph = {
-        "links": [
-            { "source": "2/2/2023", "target": "7/2/2023", "value": "5.0" },
-            { "source": "2/2/2023", "target": "7/2/2023", "value": "5.0" },
-            { "source": "7/2/2023", "target": "31/03/2023", "value": " 5.0" },
-        ],
-        "nodes": [
-
-            { "name": "2/2/2023" },
-            { "name": "7/2/2023" },
-            { "name": "31/03/2023" }
-
-        ]
-    }
-
-
-
-
-    let dummyData = {
-        "links": [
-            { "source": "2/2/2023", "target": "7/2/2023", "value": "5.0" },
-            { "source": "2/2/2023", "target": "7/2/2023", "value": "5.0" },
-            { "source": "7/2/2023", "target": "11/1/2022", "value": "5.0" },
-            { "source": "28/10/2022", "target": "11/5/2021", "value": "5.0" },
-        ],
-        "nodes": [
-            { "name": "28/10/2022" },
-            { "name": "12/9/2022" },
-            { "name": "2/2/2023" },
-            { "name": "11/1/2022" },
-            { "name": "21/01/2022" },
-            { "name": "21/05/2021" },
-            { "name": "2/3/2022" },
-            { "name": "11/5/2021" },
-            { "name": "7/10/2022" },
-            { "name": "7/9/2021" },
-            { "name": "11/5/2022" },
-            { "name": "25/05/2022" },
-            { "name": "14/03/2023" },
-            { "name": "9/8/2022" },
-            { "name": "8/9/2022" },
-            { "name": "18/07/2022" },
-            { "name": "30/10/2022" },
-            { "name": "7/2/2023" },
-            { "name": "23/03/2022" },
-            { "name": "10/11/2022" },
-            { "name": "30/08/2022" },
-            /* { "name": "1/9/2022" },
-           { "name": "23/08/2021" },
-           { "name": "14/06/2017" },
-           { "name": "26/10/2021" },
-           { "name": "24/01/2022" },
-           { "name": "17/10/2022" },
-           { "name": "15/06/2021" },
-           { "name": "5/8/2022" },
-           { "name": "22/06/2022" },
-           { "name": "18/05/2022" },
-           { "name": "4/4/2023" },
-           { "name": "7/2/2023" },
-           { "name": "14/07/2022" },
-           { "name": "15/11/2022" },
-           { "name": "2/5/2022" },
-           { "name": "2/11/2022" },
-           { "name": "17/08/2022" },
-           { "name": "6/9/2022" },
-           { "name": "8/11/2018" },
-           { "name": "17/02/2023" },
-           { "name": "18/04/2023" },
-           { "name": "30/10/2020" },
-           { "name": "21/06/2022" },
-           { "name": "24/05/2023" },
-           { "name": "14/04/2023" },
-           { "name": "2/2/2023" },
-           { "name": "7/2/2023" },
-           { "name": "7/2/2023" },
-           { "name": "21/04/2023" },
-           { "name": "17/03/2023" },
-           { "name": "24/05/2021" },
-           { "name": "16/05/2023" },
-           { "name": "19/04/2023" },
-           { "name": "3/5/2023" },
-           { "name": "3/3/2023" },
-           { "name": "8/2/2023" },
-           { "name": "2/2/2023" },
-           { "name": "15/05/2023" },
-           { "name": "10/2/2023" },
-           { "name": "10/3/2023" },
-           { "name": "21/02/2023" },
-           { "name": "21/02/2023" },
-           { "name": "3/2/2023" },
-           { "name": "15/02/2023" },
-           { "name": "9/2/2023" },
-           { "name": "25/01/2022" },
-           { "name": "26/03/2023" },
-           { "name": "8/2/2023" },
-           { "name": "11/5/2023" },
-           { "name": "3/5/2023" },
-           { "name": "14/04/2023" },
-           { "name": "22/02/2023" },
-           { "name": "10/2/2023" },
-           { "name": "15/05/2023" },
-           { "name": "2/5/2023" },
-           { "name": "7/2/2023" },
-           { "name": "19/04/2023" },
-           { "name": "26/03/2023" },
-           { "name": "9/5/2023" },
-           { "name": "18/04/2023" },
-           { "name": "3/2/2023" },
-           { "name": "14/02/2023" },
-           { "name": "10/2/2023" },
-           { "name": "25/05/2023" } */
-
-
-        ]
-    }
 
     let leadCreatedUniqueDates = responseData.map((o, i) => o['Created Date']).filter(onlyUnique)
     let oppCreatedUniqueDates = responseData.map((o, i) => o['Converted Date']).filter(onlyUnique)
     let oppClosedUniqueDates = responseData.map((o, i) => o['Oppt Close Date']).filter(onlyUnique).filter(v => v != "")
 
-    // console.log('opp close : ', oppClosedUniqueDates)
 
     let leadNodes = {
         "links": [  ...getLinks(responseData, "Created Date", "Converted Date"), 
@@ -689,29 +441,16 @@ fetch('/get_data').then(res => res.json()).then(responseData => {
             ...getNodes(leadCreatedUniqueDates),
             ...getNodes(oppCreatedUniqueDates),
             ...getNodes(oppClosedUniqueDates)
-        ].map(k => k['name']).filter(onlyUnique).map(v => { return { "name": v } })
+        ].map(k => k['name'])
+        .filter(onlyUnique)
+        .map(v => { return { "name": v } })
     }
 
     let miniLeads = {
-                        "links": leadNodes['links'],  //.slice(0,65),
+                        "links": leadNodes['links'],  
                         "nodes": leadNodes['nodes']
                     }
 
-    console.log("Freaking Main Leads : ", leadNodes)
-    console.log("Default Dummy Data : ", dummyData)
-    console.log("Default Graph Data : ", graph)
-
-    console.log("dup nodes : ", leadNodes['nodes'])
-
-    // console.log('%csankey-new.js line:703 error found in ', 'color: red;', leadNodes['links'][45] );
-
-    // drawSankey(graph)
-    // drawSankey(dummyData)
-    // drawSankey(testData)
-    // drawSankey(leadNodes)
+    // Generate sankey chart
     drawSankey(miniLeads)
-
-
-
-
 });
